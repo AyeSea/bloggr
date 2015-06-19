3e7b752a60defe81f0ea5dd1ec1f34ed9346a1eb
Andrew Chung <andrew.chung159@gmail.com>
2015-06-19 17:21:54 -0400

    signin user after successful signup and show user profile on signin

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,25 +1,45 @@
 class RegistrationsController < Devise::RegistrationsController
 
-	def create
-		@user = User.new(sign_up_params)
-		if @user.save
-			flash[:success] = "Signed up successfully!"
-			redirect_to root_path
-		else
-			flash[:danger] = "Uh oh! Something went wrong!"
-			render 'new'
+ def create
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_flashing_format?
+        sign_up(resource_name, resource)
+        #Signin user after succesful signup.
+        sign_in(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
+
+	protected
+
+		def after_sign_up_path_for(resource)
+			after_sign_in_path_for(resource)
 		end
-	end
 
 	private
 
-	def sign_up_params
-		params.require(:user).permit(:first_name, :last_name, :email, :email_confirmation, :gender, :password, 
-			                           :password_confirmation, :birthday)
-	end
+		def sign_up_params
+			params.require(:user).permit(:first_name, :last_name, :email, :email_confirmation, :gender, :password, 
+				                           :password_confirmation, :birthday)
+		end
 
-	def account_update_params
-		params.require(:user).permit(:first_name, :last_name, :email, :email_confirmatin, :gender, :password, 
-			                           :password_confirmation, :current_password, :birthday)
-	end
+		def account_update_params
+			params.require(:user).permit(:first_name, :last_name, :email, :email_confirmatin, :gender, :password, 
+				                           :password_confirmation, :current_password, :birthday)
+		end
+
 end
